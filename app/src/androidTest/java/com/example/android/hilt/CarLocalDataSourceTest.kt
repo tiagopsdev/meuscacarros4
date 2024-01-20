c78a37c5299f37018c95bd9c6591e171a8d86f20
Tiago <tiagopaivasantos.dev@gmail.com>
2024-01-20 10:23:35 -0300

    Correção do teste mudando de map para objeto na implementação do banco de dados

--- app/src/androidTest/java/com/example/android/hilt/CarLocalDataSourceTest.kt
+++ app/src/androidTest/java/com/example/android/hilt/CarLocalDataSourceTest.kt
@@ -5,11 +5,10 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.hilt.data.AppDatabase
+import com.example.android.hilt.data.Car
 import com.example.android.hilt.data.CarLocalDataSource
-import com.example.android.hilt.di.CarDbModule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -50,16 +49,16 @@ class CarLocalDataSourceTest {
 
     @Test
     fun testAddCar() {
-        val carMap = mapOf(
-            "carName" to "Car 1",
-            "kmActual" to "1000",
-            "kmOilChange" to "5000",
-            "kmWheels" to "3000",
-            "kmBreakes" to "2000",
-            "kmFilters" to "10000"
+        val testCar = Car(
+            "Car 1",
+            1000,
+            5000,
+            3000,
+            2000,
+            10000
         )
 
-        carLocalDataSource.addCar(carMap)
+        carLocalDataSource.addCar(testCar)
 
         // Retrieve cars and assert that the added car is in the list
         carLocalDataSource.getAllCars { cars ->
@@ -71,13 +70,13 @@ class CarLocalDataSourceTest {
 
     @Test
     fun testGetAllCars() {
-        // Add some cars to the database
-        val carMap1 = mapOf("carName" to "Car 1", "kmActual" to "1000", "kmOilChange" to "5000", "kmWheels" to "3000", "kmBreakes" to "2000", "kmFilters" to "10000")
-        val carMap2 = mapOf("carName" to "Car 2", "kmActual" to "2000", "kmOilChange" to "6000", "kmWheels" to "4000", "kmBreakes" to "3000", "kmFilters" to "11000")
-        val carMap3 = mapOf("carName" to "Car 3", "kmActual" to "3000", "kmOilChange" to "7000", "kmWheels" to "5000", "kmBreakes" to "4000", "kmFilters" to "12000")
-        carLocalDataSource.addCar(carMap1)
-        carLocalDataSource.addCar(carMap2)
-        carLocalDataSource.addCar(carMap3)
+        // Add some cars to the databa
+        val testCar1 = Car("Car 1", 1000,5000,3000, 2000,10000)
+        val testCar2 = Car("Car 2", 2000,6000,4000, 3000,11000)
+        val testCar3 = Car("Car 3", 3000,7000,5000, 4000,12000)
+        carLocalDataSource.addCar(testCar1)
+        carLocalDataSource.addCar(testCar2)
+        carLocalDataSource.addCar(testCar3)
 
         // Retrieve all cars and assert the expected count
         carLocalDataSource.getAllCars { cars ->
@@ -88,8 +87,8 @@ class CarLocalDataSourceTest {
     @Test
     fun testFindCarById() {
         // Add a car to the database
-        val carMap = mapOf("carName" to "Car 1", "kmActual" to "1000", "kmOilChange" to "5000", "kmWheels" to "3000", "kmBreakes" to "2000", "kmFilters" to "10000")
-        carLocalDataSource.addCar(carMap)
+        val testCar = Car("Car 1", 1000,5000,3000,2000,10000)
+        carLocalDataSource.addCar(testCar)
 
         // Find the car by ID and assert its properties
         carLocalDataSource.findCarById(1) { car ->
@@ -101,8 +100,8 @@ class CarLocalDataSourceTest {
     @Test
     fun testDeleteCar() {
         // Add a car to the database
-        val carMap = mapOf("carName" to "Car 1", "kmActual" to "1000", "kmOilChange" to "5000", "kmWheels" to "3000", "kmBreakes" to "2000", "kmFilters" to "10000")
-        carLocalDataSource.addCar(carMap)
+        val testCar = Car("Car 1", 1000,5000,3000,2000,10000)
+        carLocalDataSource.addCar(testCar)
 
         // Find and delete the car
         carLocalDataSource.findCarById(1) { car ->
